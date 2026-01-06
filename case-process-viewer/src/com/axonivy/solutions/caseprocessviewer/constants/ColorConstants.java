7ac2c0b0ee6d12ca9c27af3d7f9faca77ad0dc0d
vhhoang-axonivy <hoang.vuhuy@axonactive.com>
2026-01-06 11:40:02 +0700

    Set up dynamic color

--- case-process-viewer/src/com/axonivy/solutions/caseprocessviewer/constants/ColorConstants.java
+++ case-process-viewer/src/com/axonivy/solutions/caseprocessviewer/constants/ColorConstants.java
@@ -5,7 +5,7 @@ import ch.ivyteam.ivy.environment.Ivy;
 public class ColorConstants {
 //  public static final String PASSED_COLOR = "#47C46B";
   public static final String PASSED_COLOR = Ivy.var().get("caseProcessViewer.passedColor");
-  public static final String ACTIVE_COLOR = "#3B82F6";
-  public static final String FREQUENCY_COLOR = "#47C46B";
-  public static final String FREQUENCY_TEXT_COLOR = "#000000";
+  public static final String ACTIVE_COLOR = Ivy.var().get("caseProcessViewer.activeColor");
+  public static final String FREQUENCY_COLOR = Ivy.var().get("caseProcessViewer.frequencyColor");
+  public static final String FREQUENCY_TEXT_COLOR = Ivy.var().get("caseProcessViewer.frequencyTextColor");
 }
