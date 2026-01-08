60cd82d707c4e65ee7665cae328464bd69f4cd1c
vhhoang-axonivy <hoang.vuhuy@axonactive.com>
2026-01-08 16:59:11 +0700

    Handle feedback

--- case-process-viewer/src/com/axonivy/solutions/caseprocessviewer/constant/ColorVariableConstants.java
+++ case-process-viewer/src/com/axonivy/solutions/caseprocessviewer/constant/ColorVariableConstants.java
@@ -1,8 +1,9 @@
 package com.axonivy.solutions.caseprocessviewer.constant;
 
 public class ColorVariableConstants {
-  public static final String PASSED_COLOR = "caseProcessViewer.passedColor";
-  public static final String ACTIVE_COLOR = "caseProcessViewer.activeColor";
-  public static final String FREQUENCY_COLOR = "caseProcessViewer.frequencyColor";
-  public static final String FREQUENCY_TEXT_COLOR = "caseProcessViewer.frequencyTextColor";
+  public static final String CASE_PROCESS_VIEWER_GROUP_ID = "com.axonivy.solutions.caseProcessViewer";
+  public static final String PASSED_COLOR = CASE_PROCESS_VIEWER_GROUP_ID + ".passedColor";
+  public static final String ACTIVE_COLOR = CASE_PROCESS_VIEWER_GROUP_ID + ".activeColor";
+  public static final String FREQUENCY_COLOR = CASE_PROCESS_VIEWER_GROUP_ID + ".frequencyColor";
+  public static final String FREQUENCY_TEXT_COLOR = CASE_PROCESS_VIEWER_GROUP_ID + ".frequencyTextColor";
 }
