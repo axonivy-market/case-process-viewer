b22fb58cd78a925b72bf137348a2699ffb0e121e
phhung-axonivy <hung.pham@axonactive.com>
2026-06-05 09:13:29 +0700

    MARP-4499 Bug in Case Process Viewer 13.2.0

--- case-process-viewer-test/src_test/com/axonivy/solutions/caseprocessviewer/test/it/WebBaseTest.java
+++ case-process-viewer-test/src_test/com/axonivy/solutions/caseprocessviewer/test/it/WebBaseTest.java
@@ -2,6 +2,7 @@ package com.axonivy.solutions.caseprocessviewer.test.it;
 
 import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -9,9 +10,14 @@ import com.axonivy.ivy.webtest.engine.EngineUrl;
 
 public class WebBaseTest {
   protected static final String PROCESS_TEST_PATH = "/case-process-viewer-test/19A52DA5B971F827/start.ivp";
+  protected static final String PROCESS_DEMO_PATH = "/case-process-viewer-demo/199DB82E409AE3B0/start.ivp";
 
   protected void startProcessTest() {
     open(EngineUrl.createProcessUrl(PROCESS_TEST_PATH));
+  }
+
+  protected void startProcessDemo() {
+    open(EngineUrl.createProcessUrl(PROCESS_DEMO_PATH));
   }
 
   protected void openTasksPage() {
@@ -23,13 +29,15 @@ public class WebBaseTest {
   }
 
   protected void loginWithUser(String username, String password) {
+    openLoginPage();
     $("#loginForm\\:userName").shouldBe(visible).setValue(username);
     $("#loginForm\\:password").shouldBe(visible).setValue(password);
     $("#loginForm\\:login").shouldBe(enabled).click();
   }
 
-  protected void startFirstTask() {
-    $("button#tasksForm\\:tasks\\:0\\:taskActionsBtn").shouldBe(enabled).click();
-    $("a#tasksForm\\:tasks\\:0\\:startTaskBtn").shouldBe(enabled).click();
+  protected void startTask(String taskName) {
+    openTasksPage();
+    $("#tasksForm\\:tasks_data").$$("tr td span").findBy(text(taskName)).shouldBe(visible).shouldBe(enabled).click();
+    $("#actionMenuForm\\:taskStartBtn").shouldBe(enabled).click();
   }
 }
