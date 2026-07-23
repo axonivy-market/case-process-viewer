dd4012e2e5ccd2f8ca80ed8043f9a6ca2390524a
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-07-23 18:06:25 +0700

    Update BaseTest.java

--- case-process-viewer-utils-test/src_test/com/axonivy/utils/caseprocessviewer/test/ut/BaseTest.java
+++ case-process-viewer-utils-test/src_test/com/axonivy/utils/caseprocessviewer/test/ut/BaseTest.java
@@ -5,7 +5,7 @@ public class BaseTest {
   protected static final String TEST_PROCESS_RAW_PID = "19A52DA5B971F827";
   protected static final String TEST_EXT_PID = "19A52DA5B971F827-EXT";
   protected static final String TEST_MODULE_NAME = "case-process-viewer-utils-test";
-  protected static final String TEST_IFRAME_SOURCE_URL = "/test/faces/view/case-process-viewer/case-process-viewer.xhtml?server=localhost%3A8080&app=test&pmv=Test&file=ProcessTest.p.json";
+  protected static final String TEST_IFRAME_SOURCE_URL = "/test/faces/view/case-process-viewer-utils/case-process-viewer.xhtml?server=localhost%3A8080&app=test&pmv=Test&file=ProcessTest.p.json";
   protected static final String MODULE_URL = "Test";
   protected static final String PROJECT_PATH = "ProcessTest.p.json";
 
